2f87a45b56b8d2e3be8614f76fb467d0f2377144
Michael Frister <michael.frister@friday.de>
2019-10-15 11:15:30 +0200

    Add flake8 and isort checks and fix the problems

--- bot/main.py
+++ bot/main.py
@@ -1,11 +1,14 @@
+import logging
+
 import click
 import gitlab
-import logging
 
 log = logging.getLogger(__name__)
 
+
 def main():
     cli(auto_envvar_prefix='APP')
+
 
 @click.command()
 @click.option(
@@ -19,7 +22,7 @@ def main():
 @click.option('--url', help='The GitLab instance URL.')
 @click.option('--private-token', help='A GitLab private access token.')
 def cli(project_id, source_branch, target_branch,
-    url=None, private_token=None):
+        url=None, private_token=None):
     """Diff two branches in a given project and output whether their content
     differs.
 
@@ -34,11 +37,11 @@ def cli(project_id, source_branch, target_branch,
     if url is not None or private_token is not None:
         gl = gitlab.Gitlab(url, private_token=private_token)
     else:
-        log.info("Using python-gitlab config file")
+        log.info('Using python-gitlab config file')
         gl = gitlab.Gitlab.from_config()
 
     project = gl.projects.get(project_id)
     comparison = project.repository_compare(source_branch, target_branch)
     diff_has_content = len(comparison['diffs']) > 0
-    log.info("Branches {} and {} differ: {}".format(
+    log.info('Branches {0} and {1} differ: {2}'.format(
         source_branch, target_branch, diff_has_content))
