4bb9c10b4c622c36b2ec13c62dae7d2828ebc8a8
Michael Frister <michael.frister@friday.de>
2019-10-15 14:57:10 +0200

    Create MR if branch contents differ

--- bot/main.py
+++ bot/main.py
@@ -19,9 +19,11 @@ def main():
     help='The source branch to be merged into the target branch')
 @click.option(
     '--target-branch', '-t', required=True, help='The target branch')
+@click.option('--label', 'labels', default=[], multiple=True,
+              help='Label to attach to created MRs (multiple allowed).')
 @click.option('--url', help='The GitLab instance URL.')
 @click.option('--private-token', help='A GitLab private access token.')
-def cli(project_id, source_branch, target_branch,
+def cli(project_id, source_branch, target_branch, labels,
         url=None, private_token=None):
     """Diff two branches in a given project and output whether their content
     differs.
@@ -44,14 +46,27 @@ def cli(project_id, source_branch, target_branch,
 
     existing_mr = existing_merge_request(project, source_branch, target_branch)
     if existing_mr is not None:
-        log.info('Merge request already exists, stopping. MR: {0}',
-                 existing_mr.web_url)
+        log.info('{0} -> {1}: Merge request already exists, stopping. '
+                 'MR: {2}'.format(
+                    source_branch, target_branch, existing_mr.web_url))
         return
 
     branch_content_differs = does_branch_content_differ(
         project, source_branch, target_branch)
-    log.info('Branches {0} and {1} differ: {2}'.format(
-        source_branch, target_branch, branch_content_differs))
+    if not branch_content_differs:
+        log.info('{0} -> {1}: Branch contents are the same, stopping.'.format(
+                 source_branch, target_branch))
+        return
+
+    mr_title = '⛵️ {0} to {1}'.format(source_branch, target_branch)
+    created_mr = project.mergerequests.create({
+        'source_branch': source_branch,
+        'target_branch': target_branch,
+        'title': mr_title,
+        'labels': labels})
+
+    log.info('{0} -> {1}: Created MR: {2}'.format(
+             source_branch, target_branch, created_mr.web_url))
 
 
 def does_branch_content_differ(project, source, target):
