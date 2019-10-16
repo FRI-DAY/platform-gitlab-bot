705e8f7b9bd53533864bf3f0c2d99209dccb2398
Michael Frister <michael.frister@friday.de>
2019-10-16 16:10:33 +0200

    Add very basic HTTP server creating an MR on POST
    Doesn't take any parameters yet or support different branches.

--- bot/main.py
+++ bot/main.py
@@ -2,6 +2,7 @@ import logging
 
 import click
 import gitlab
+from aiohttp import web
 
 from bot.branch_promotion import BranchPromoter
 
@@ -13,6 +14,8 @@ def main():
 
 
 @click.command()
+@click.option('--server/--no-server', '-d', default=False,
+              help='Run an HTTP server receiving webhooks')
 @click.option(
     '--project-id', '-p', required=True, type=int,
     help='The numeric id of the GitLab project to work on.')
@@ -25,7 +28,7 @@ def main():
               help='Label to attach to created MRs (multiple allowed).')
 @click.option('--url', help='The GitLab instance URL.')
 @click.option('--private-token', help='A GitLab private access token.')
-def cli(project_id, source_branch, target_branch, labels,
+def cli(server, project_id, source_branch, target_branch, labels,
         url=None, private_token=None):
     """Diff two branches in a given project and output whether their content
     differs.
@@ -45,5 +48,28 @@ def cli(project_id, source_branch, target_branch, labels,
         gl = gitlab.Gitlab.from_config()
 
     promoter = BranchPromoter(gl)
-    promoter.create_merge_request(
-        project_id, source_branch, target_branch, labels)
+
+    if server:
+        run_server(promoter, project_id, source_branch, target_branch, labels)
+    else:
+        promoter.create_merge_request(
+            project_id, source_branch, target_branch, labels)
+
+
+def run_server(promoter, project_id, source_branch, target_branch, labels):
+    promote_branch_handler = create_promote_branch_handler(
+        promoter, project_id, source_branch, target_branch, labels)
+
+    app = web.Application()
+    app.add_routes([web.post('/', promote_branch_handler)])
+    web.run_app(app)
+
+
+def create_promote_branch_handler(
+        promoter, project_id, source_branch, target_branch, labels):
+
+    async def handle_promote_branch(request):
+        promoter.create_merge_request(
+            project_id, source_branch, target_branch, labels)
+        return web.Response(text='Done.')
+    return handle_promote_branch
