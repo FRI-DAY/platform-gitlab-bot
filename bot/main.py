28f9660e3fd43c57ffcb3d4d7beaa6bf42786d31
Michael Frister <michael.frister@friday.de>
2019-10-16 15:46:47 +0200

    Extract branch promotion into module
    Preparation for adding an HTTP server

--- bot/main.py
+++ bot/main.py
@@ -3,6 +3,8 @@ import logging
 import click
 import gitlab
 
+from bot.branch_promotion import BranchPromoter
+
 log = logging.getLogger(__name__)
 
 
@@ -42,40 +44,6 @@ def cli(project_id, source_branch, target_branch, labels,
         log.info('Using python-gitlab config file')
         gl = gitlab.Gitlab.from_config()
 
-    project = gl.projects.get(project_id)
-
-    existing_mr = existing_merge_request(project, source_branch, target_branch)
-    if existing_mr is not None:
-        log.info('{0} -> {1}: Merge request already exists, stopping. '
-                 'MR: {2}'.format(
-                    source_branch, target_branch, existing_mr.web_url))
-        return
-
-    branch_content_differs = does_branch_content_differ(
-        project, source_branch, target_branch)
-    if not branch_content_differs:
-        log.info('{0} -> {1}: Branch contents are the same, stopping.'.format(
-                 source_branch, target_branch))
-        return
-
-    mr_title = '⛵️ {0} to {1}'.format(source_branch, target_branch)
-    created_mr = project.mergerequests.create({
-        'source_branch': source_branch,
-        'target_branch': target_branch,
-        'title': mr_title,
-        'labels': labels})
-
-    log.info('{0} -> {1}: Created MR: {2}'.format(
-             source_branch, target_branch, created_mr.web_url))
-
-
-def does_branch_content_differ(project, source, target):
-    comparison = project.repository_compare(source, target)
-    return len(comparison['diffs']) > 0
-
-
-def existing_merge_request(project, source, target):
-    existing = project.mergerequests.list(
-        state='opened', source_branch=source, target_branch=target)
-
-    return existing[0] if existing else None
+    promoter = BranchPromoter(gl)
+    promoter.create_merge_request(
+        project_id, source_branch, target_branch, labels)
