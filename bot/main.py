cc6cfa0d0ed14b4f8a55fa187f24d7c8fd327319
Michael Frister <michael.frister@friday.de>
2019-10-22 11:41:31 +0200

    Use separate CLI commands: once and server
    server doesn't need a project id - it receives the project id via the
    Webhook request.
    
    Also update the Dockerfile to run the server by default.

--- bot/main.py
+++ bot/main.py
@@ -13,17 +13,20 @@ def main():
     cli(auto_envvar_prefix='APP')
 
 
-@click.command()
-@click.option('--server/--no-server', '-d', default=False,
-              help='Run an HTTP server receiving webhooks')
-@click.option(
-    '--project-id', '-p', required=True, type=int,
-    help='The numeric id of the GitLab project to work on.')
+@click.group()
 @click.option('--url', help='The GitLab instance URL.')
 @click.option('--private-token', help='A GitLab private access token.')
-def cli(server, project_id, url=None, private_token=None):
-    """Diff two branches in a given project and output whether their content
-    differs.
+@click.pass_context
+def cli(ctx, url=None, private_token=None):
+    """
+    A GitLab bot. For now, it compares a source and a target branch and,
+    if their contents differ, it creates a merge request from the source to
+    the target branch. This is useful for promoting changes from one branch to
+    another, e.g. for managing configuration for different infrastructure
+    environments.
+
+    Configuration happens via a configuration file in the project's default
+    branch, called `gitlab-bot.yml`.
 
     In addition to command line flags, you can configure this tool via
     environment variables by using the long-form arguments, making them
@@ -39,9 +42,24 @@ def cli(server, project_id, url=None, private_token=None):
         log.info('Using python-gitlab config file')
         gl = gitlab.Gitlab.from_config()
 
-    runner = Runner(gl)
+    ctx.ensure_object(dict)
+    ctx.obj['gitlab'] = gl
 
-    if server:
-        Server(runner).run()
-    else:
-        runner.run(project_id)
+
+@cli.command()
+@click.option(
+    '--project-id', '-p', required=True, type=int,
+    help='The numeric id of the GitLab project to work on.')
+@click.pass_context
+def once(ctx, project_id):
+    """Run the bot tasks once on a specific GitLab project."""
+    runner = Runner(ctx.obj['gitlab'])
+    runner.run(project_id)
+
+
+@cli.command()
+@click.pass_context
+def server(ctx):
+    """Run the bot as an HTTP server reacting to webhooks from GitLab."""
+    runner = Runner(ctx.obj['gitlab'])
+    Server(runner).run()
